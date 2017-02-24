9aa8d7e3375614defe864b1e8aa93e93fb8e9b1e
jdvalera <johnlemuelvalera@yahoo.com>
2017-02-24 15:21:45 -0800

    Added constructor to take in File for Artifact class. Changed createID to setID. Add method to get file extension. Added getID getter method

--- src/Artifact.java
+++ src/Artifact.java
@@ -1,38 +1,76 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Artifact {
 	
 	String artifactID;
+	File sourceFile;
 	
-	public String createID(String content) {
+	public Artifact(File f) {
+		sourceFile = f;
+		setID();
+	}
+	
+	public void setID() {
 		
 		int total = 0;
-		int length = content.length();
-		
-		for(int i = 0; i<length; i++ ) {
-			
-			byte ascii = (byte) content.charAt(i);
-			
-			if(i%4 == 0) {
-				total += ascii*1;
-			} else if (i%4 == 1) {
-				total += ascii*3;
-			} else if (i%4 == 2) {
-				total += ascii*11;
-			} else if (i%4 == 3) {
-				total += ascii*17;
+		int length = (int) sourceFile.length();
+		String ext = getFileExtension(sourceFile);
+		try {
+			InputStream input = new FileInputStream(sourceFile);
+			int data = input.read();
+			int i = 0;
+
+			while(data != -1) {
+				if(i%4 == 0) {
+					total += data*1;
+				} else if (i%4 == 1) {
+					total += data*3;
+				} else if (i%4 == 2) {
+					total += data*11;
+				} else if (i%4 == 3) {
+					total += data*17;
+				}
+				data = input.read();
+				i++;
 			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		String output = "" + total + "." + length;
+		 artifactID =  "" + total + "." + length + "." + ext;
 		
-		return output;
+	}
+	
+	public String getID() {
+		return artifactID;
+	}
+	
+	public String getFileExtension(File file) {
+	    String name = file.getName();
+	    try {
+	        return name.substring(name.lastIndexOf(".") + 1);
+	    } catch (Exception e) {
+	        return "";
+	    }
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Artifact a = new Artifact();
+		File test = new File("E:\\Users\\John\\Desktop\\Dropbox\\CECS 543\\Project", "text.txt");
+		Artifact a = new Artifact(test);
 		
-		System.out.println(a.createID("HELLO WORLD"));
+		
+		System.out.println(a.getID());
+		
+		//System.out.println(a.createID("HELLO WORLD"));
 	}
 
 
