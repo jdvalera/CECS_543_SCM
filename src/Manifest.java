6aa5c348000279db6952634b92d3a93056bff7ef
jdvalera <johnlemuelvalera@yahoo.com>
2017-02-25 01:35:35 -0800

    Added functionality to Manifest to allow it to write content to file

--- src/Manifest.java
+++ src/Manifest.java
@@ -1,20 +1,69 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class Manifest {
 	
 	String projectName = "Vix-1";
-	String creationTime;
-	String userCmd;
-	String srcPath;
-	String targetPath;
+	String creationTime = "";
+	String userCmd = "";
+	String srcPath = "";
+	String targetPath = "";
+	String fileName = "";
 	ArrayList<Artifact> artifacts;
+	File directory;
 	
 	public Manifest(String path) {
-		File directory = new File(path + File.separator + "activity");
+		creationTime = new SimpleDateFormat("MM-dd-yyyy-HH-mm")
+				.format(new java.util.Date());
+		directory = new File(path + File.separator + "activity");
 		if(!directory.exists()) {
 			directory.mkdir();
 		}
+		fileName = "Manifest-" + creationTime + ".txt";
+		createManifestFile();
+	}
+	
+	public String[] createManifestContent() {
+		
+		String[] content = {projectName, creationTime, userCmd, srcPath, targetPath};
+	
+		
+		return content;
+	}
+	
+	public void createManifestFile() {
+		
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(directory.getPath() + File.separator + fileName);
+			bw = new BufferedWriter(fw);
+			String[] content = createManifestContent();
+			for(String s : content) {
+				
+				bw.write(s);
+				bw.newLine();
+			}	
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				bw.close();
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
+		}
+		
 	}
 	
 	public void setTime(String t) {
