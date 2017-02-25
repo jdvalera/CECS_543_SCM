2d176266d6e6745c27c934ba30462c83eeb01787
jdvalera <johnlemuelvalera@yahoo.com>
2017-02-25 12:23:53 -0800

    Added setters and getters for ManifestFields

--- src/ManifestFields.java
+++ src/ManifestFields.java
@@ -12,6 +12,19 @@ public class ManifestFields {
 	ArrayList<Artifact> artifacts;
 	File directory;
 	
+	public ManifestFields() {
+
+		projectName = "Vix-1";
+		creationTime = "";
+		userCmd = "";
+		srcPath = "";
+		targetPath = "";
+		fileName = "";
+		artifacts = new ArrayList<Artifact>();
+		directory = null;
+		
+	}
+	
 	public  ManifestFields(String pN, String cT, String uC, String sP, String tP,
 			String fN, ArrayList<Artifact> af,File dir) {
 		
@@ -24,5 +37,69 @@ public class ManifestFields {
 		artifacts = af;
 		directory = dir;
 		
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(String creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public String getUserCmd() {
+		return userCmd;
+	}
+
+	public void setUserCmd(String userCmd) {
+		this.userCmd = userCmd;
+	}
+
+	public String getSrcPath() {
+		return srcPath;
+	}
+
+	public void setSrcPath(String srcPath) {
+		this.srcPath = srcPath;
+	}
+
+	public String getTargetPath() {
+		return targetPath;
+	}
+
+	public void setTargetPath(String targetPath) {
+		this.targetPath = targetPath;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public ArrayList<Artifact> getArtifacts() {
+		return artifacts;
+	}
+
+	public void setArtifacts(ArrayList<Artifact> artifacts) {
+		this.artifacts = artifacts;
+	}
+
+	public File getDirectory() {
+		return directory;
+	}
+
+	public void setDirectory(File directory) {
+		this.directory = directory;
 	}
 }
