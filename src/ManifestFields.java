eb59a7d100e6b182c502bf6518bbc743ff4754c0
jdvalera <johnlemuelvalera@yahoo.com>
2017-02-25 01:45:39 -0800

    Added ManifestFields class for passing values to Manifest class

---
+++ src/ManifestFields.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class ManifestFields {
+	
+	String projectName = "Vix-1";
+	String creationTime = "";
+	String userCmd = "";
+	String srcPath = "";
+	String targetPath = "";
+	String fileName = "";
+	ArrayList<Artifact> artifacts;
+	File directory;
+	
+}
